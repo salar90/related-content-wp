97efadc25c809c626860584470b0d18e09c5c15e
Salar Gholizadeh <salar.g@gmail.com>
2020-05-29 18:06:10 +0430

    add template

---
+++ template.php
@@ -0,0 +1,24 @@
+<?php
+if($related_posts_query->have_posts()):
+?>
+<div class="sg_related_posts">
+<h2 class="sg_related_posts_title"><?php apply_filters('sg_related_content_widget_title', _e('You might also read', 'related-content')) ?></h2>
+<ul class="sg_related_posts_list">
+<?php 
+    while($related_posts_query->have_posts()):
+        $related_posts_query->the_post();
+?>
+<li>
+    <a href="<?php the_permalink() ?>">
+        <?php the_post_thumbnail() ?>
+        <?php the_title() ?>
+    </a>
+</li>
+
+<?php endwhile ?>
+</ul>
+</div>
+
+<?php else: ?>
+no related posts
+<?php endif ?>
