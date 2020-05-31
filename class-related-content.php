719154e57e0c8807d69a71890005ec2fd850a5cf
Salar Gholizadeh <salar.g@gmail.com>
2020-06-01 00:59:46 +0430

    implement related extractor

--- class-related-content.php
+++ class-related-content.php
@@ -47,17 +47,101 @@ class SG_Related_Content{
         return $output;
     }
 
-    private function get_related_posts_query()
+    private function get_related_posts_query($count = 5)
     {
         global $post;
         $tag_terms = wp_get_post_terms($post->ID);
-        $cat_terms = wp_get_post_terms($post->ID, 'category');
+
         
+        $cat_terms = wp_get_post_terms($post->ID, 'category');
+
+        $countTags = array_reduce($tag_terms,function($count,$term){
+            $count += $term->count;
+            return $count;
+        }, 0);
+
+        $countCats = array_reduce($cat_terms,function($count,$term){
+            $count += $term->count;
+            return $count;
+        }, 0);
+        
+
+        $tag_terms = array_map(function($term)use($countTags){
+            return [
+                'term_id' => $term->term_id,
+                'name' => $term->name,
+                'power' => intval((1 - $term->count / $countTags) * 100) * 1 
+            ];
+        }, $tag_terms);
+
+        $cat_terms = array_map(function($term)use($countCats){
+            return [
+                'term_id' => $term->term_id,
+                'name' => $term->name,
+                'power' => intval((1 - $term->count / $countCats) * 100) * 1.5
+            ];
+        }, $cat_terms);
+        
+        
+        $posts = [];
+        foreach($tag_terms as $term){
+            $args = [
+                'posts_per_page' => 100,
+                'tag_id' => $term['term_id'],
+                'fields' => 'ids'
+            ];
+            $query = new WP_Query($args);
+            
+            foreach($query->posts as $post){
+                if(isset($posts[$post])){
+                    $posts[$post]['power'] += $term['power'];
+                }else{
+                    $posts[$post] = [
+                        'id' => $post,
+                        'power' => $term['power']
+                    ];
+                }
+            }
+        }
+
+        foreach($cat_terms as $term){
+            $args = [
+                'posts_per_page' => 100,
+                'tag_id' => $term['term_id'],
+                'fields' => 'ids'
+            ];
+            $query = new WP_Query($args);
+            foreach($query->posts as $post){
+                if(isset($posts[$post])){
+                    $posts[$post]['power'] += $term['power'];
+                }else{
+                    $posts[$post] = [
+                        'id' => $post,
+                        'power' => $term['power']
+                    ];
+                }
+            }
+        }
+
+        $post_limit = 5;
+
+        usort($posts, function($a, $b){
+            return $b['power'] - $a['power'];
+        });
+        $posts = array_slice($posts,0, $post_limit);
+        $post_ids = array_map(function($post){
+            return $post['id'];
+        }, $posts);
+
+        
+
         $query_args = [
-            'post__in' => [1]
+            'post__in' => $post_ids,
+            'posts_per_page' => $post_limit
         ];
+        
         $query = new WP_Query($query_args);
-        // var_dump($query->posts);
+        
         return $query;
     }
 
