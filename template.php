c9754a90a7eab9dbec3fc39005579fd17d9e06c6
Salar Gholizadeh <salar.g@gmail.com>
2020-06-05 01:52:30 +0430

    fix styles

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
 if($related_posts_query->have_posts()):
 ?>
 <div class="sg_related_posts">
-<h2 class="sg_related_posts_title"><?php apply_filters('sg_related_content_widget_title', _e('You might also read', 'related-content')) ?></h2>
+<h3 class="sg_related_posts_title"><?php apply_filters('sg_related_content_widget_title', _e('You might also read', 'related-content')) ?></h3>
 <ul class="sg_related_posts_list">
 <?php 
     while($related_posts_query->have_posts()):
